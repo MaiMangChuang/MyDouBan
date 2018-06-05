9867d7bc03e02d5cbe5c0ef29dd6d718c86ae12d
maimanchuang <913133097@qq.com>
2018-06-05 20:12:05 +0800

    功能基本完成

--- app/src/main/java/com/example/mydouban/util/MyObservable.java
+++ app/src/main/java/com/example/mydouban/util/MyObservable.java
@@ -1,6 +1,7 @@
 package com.example.mydouban.util;
 
 import rx.Observable;
+import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -9,8 +10,18 @@ import rx.schedulers.Schedulers;
  * 创建人：maimanchuang
  * 创建时间：2018/5/30 20:09
  */
-public class MyObservable {
+public class MyObservable<T> extends Observable<T> {
 
 
-
+    /**
+     * Creates an Observable with a Function to execute when it is subscribed to.
+     * <p>
+     * <em>Note:</em> Use {@link #unsafeCreate(OnSubscribe)} to create an Observable, instead of this constructor,
+     * unless you specifically have a need for inheritance.
+     *
+     * @param f {@link OnSubscribe} to be executed when {@link #subscribe(Subscriber)} is called
+     */
+    protected MyObservable(OnSubscribe f) {
+        super(f);
+    }
 }
